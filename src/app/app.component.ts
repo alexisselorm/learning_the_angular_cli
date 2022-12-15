fdf24a7b9f7f6ccca49b96f6c4fa3c7e447ad0f2
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-15 18:06:31 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sybrex-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'learning_the_angular_cli';
+}
