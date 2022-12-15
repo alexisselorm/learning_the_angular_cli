8a3f4166bb9eb2289fcc4da6b88ccada230dc83b
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-15 18:33:39 +0000

    'Particles js and angular.json config'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+declare var particlesJS: any;
 @Component({
   selector: 'sybrex-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'learning_the_angular_cli';
+
+  ngOnInit() {
+    particlesJS.load('particles', 'assets/particles.json', () => {
+      console.log('particles loaded');
+    });
+  }
 }
