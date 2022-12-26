c86fb388f1ddf7438565a62b2306fc6f9e8b5f0a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-26 09:12:49 +0000

    'Creating users'

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'sybrex-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loading = false;
+  constructor(private afAuth: AngularFireAuth) {}
 
+  ngOnInit(): void {}
+  async onSubmit(form: NgForm) {
+    this.loading = true;
+    const { email, password, firstName, lastName } = form.value;
+    try {
+      const resp = await this.afAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await resp.user?.updateProfile({
+        displayName: `${firstName} ${lastName}`,
+      });
+      form.reset();
+    } catch (err) {
+      console.log(err);
+    }
+    this.loading = false;
+  }
 }
