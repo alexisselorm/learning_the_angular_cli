a807074189c32b87b9a85d55fb204b55a7dfd313
Alexis Gbeckor-Kove <AGKove@sybrin.co.za>
2022-12-26 16:25:20 +0200

    Logout user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './core/auth.service';
 declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'grid-dashboard';
+
+  constructor(public auth: AuthService) {}
 
   ngOnInit() {
     particlesJS.load('particles', 'assets/particles.json', () => {
       console.log('callback - particles.js config loaded');
     });
+  }
+  logout() {
+    this.auth.logout();
   }
 }
--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private router: Router, private afAuth: AngularFireAuth) {}
 
-  constructor() { }
+  logout() {
+    this.afAuth.signOut();
+    this.router.navigate(['']);
+  }
+  authIsLoggedIn() {
+    return !!this.afAuth.currentUser;
+  }
 }
