508815a67e77f2e9795c7ca3d572cb3260f50fd8
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-12-26 17:16:04 +0200

    Using angular route guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
+import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, ProfileComponent],
@@ -19,7 +20,7 @@ import { ProfileComponent } from './profile/profile.component';
     AngularFireAuthModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [AngularFireAuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
